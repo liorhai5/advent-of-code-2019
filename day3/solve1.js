f1c8b40608c6c30ced5b96cdd54d6208e6b75f1b
liorha <liorha@wix.com>
2019-12-03 08:57:44 +0200

    day 3

---
+++ day3/solve1.js
@@ -0,0 +1,57 @@
+const solve = (input) => {
+    const path1 = input[0].split(',');
+    const path2 = input[1].split(',');
+    const pointsPath1 = pathToPoints(path1);
+    const pointsPath2 = pathToPoints(path2);
+    const intersections = findIntersections(pointsPath1, pointsPath2);
+    return findShortestDistance(intersections);
+};
+
+const pathToPoints = path => {
+    const points = [];
+    let x = 0;
+    let y = 0;
+    let dir = '';
+    let steps = 0;
+    for (let i = 0; i < path.length; i++) {
+        dir = path[i][0];
+        steps = parseInt(path[i].substr(1).trim(), 10);
+        while (steps > 0) {
+            switch (dir) {
+                case 'U': y++; break;
+                case 'D': y--; break;
+                case 'R': x++; break;
+                case 'L': x--; break;
+            }
+            points.push(`${x},${y}`);
+            steps--;
+        }
+    }
+    return points;
+};
+
+const findIntersections = (pointsPath1, pointsPath2) => {
+    const setB = new Set(pointsPath2);
+    return [...new Set(pointsPath1)].filter(x => setB.has(x));
+};
+
+const manhattenDistance = (x1, y1, x2, y2) => {
+    return Math.abs(x2 - x1) + Math.abs(y2 - y1);
+};
+
+const findShortestDistance = intersections => {
+    let minDist = 999999999;
+    for (let i = 0; i < intersections.length; i++) {
+        const cords = intersections[i].split(',');
+        minDist = Math.min(minDist, manhattenDistance(0, 0, parseInt(cords[0], 10), parseInt(cords[1], 10)));
+    }
+    return minDist;
+};
+
+module.exports = {
+    solve,
+    pathToPoints,
+    findIntersections
+};
+
+// RESULT: 273
---
+++ day3/solve2.js
@@ -0,0 +1,27 @@
+const {pathToPoints, findIntersections} = require('./solve1');
+
+const solve = (input) => {
+    const path1 = input[0].split(',');
+    const path2 = input[1].split(',');
+    const pointsPath1 = pathToPoints(path1);
+    const pointsPath2 = pathToPoints(path2);
+    const intersections = findIntersections(pointsPath1, pointsPath2);
+    return findShortestDistance(pointsPath1, pointsPath2, intersections);
+};
+
+const findShortestDistance = (pointsPath1, pointsPath2, intersections) => {
+    let minDist = 999999999;
+    for (let i = 0; i < intersections.length; i++) {
+        const steps1 = pointsPath1.findIndex(p => p === intersections[i]) + 1;
+        const steps2 = pointsPath2.findIndex(p => p === intersections[i]) + 1;
+        minDist = Math.min(minDist, steps1 + steps2);
+    }
+    return minDist;
+};
+
+
+module.exports = {
+    solve
+};
+
+// RESULT: 15622
